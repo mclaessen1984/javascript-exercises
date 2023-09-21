044a4e7ab41115c37a3cebc0c59490137d3b98da
Maurice Claessen <mc1984@gmail.com>
2023-09-21 17:33:18 +0200

    Completed exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    // first argument is the original array
+    let array = arguments[0];
 
+    // iterate through any other arguments
+    for (i = 1; i < arguments.length; i++) {
+        const index = array.indexOf(arguments[i]);
+
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
