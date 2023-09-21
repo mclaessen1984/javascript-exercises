aa4a2ac6751ba996579b90b0cdc9dc1b9655167d
Maurice Claessen <mc1984@gmail.com>
2023-09-21 17:01:49 +0200

    Completed exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    let returnString = "";
 
+    if (n < 0) {
+        return "ERROR";
+    }
+    
+    for (let i=0; i<n; i++) {
+        returnString += string;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
