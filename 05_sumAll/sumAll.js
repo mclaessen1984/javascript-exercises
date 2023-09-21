1993ebbb4e45eabc15e6d0e979a24a80c9cdfa07
Maurice Claessen <mc1984@gmail.com>
2023-09-21 17:44:24 +0200

    Completed exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    const low = a < b ? a : b;
+    const high = a > b ? a : b;
+
+    var sum = 0;
+
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
