069f133947342b3708d04335cbcfed4baedab5ca
Maurice Claessen <mc1984@gmail.com>
2023-09-25 21:25:00 +0200

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0)
+        return "OOPS";
 
+    let fibo = [1, 1];
+
+    while (fibo.length < n) {
+        fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2]);
+    }
+    return fibo[fibo.length - 1];
 };
 
 // Do not edit below this line
