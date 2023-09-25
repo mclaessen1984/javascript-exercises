e0eef43ddf29054bf96d6e51ca8638a378495c9a
Maurice Claessen <mc1984@gmail.com>
2023-09-25 20:43:36 +0200

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, number) => { return sum + number }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((product, number) => product * number);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for (let i=1; i<=a; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
