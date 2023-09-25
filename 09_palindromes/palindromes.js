9622b057f2f8de5e1426531e0dffa118e4ce9ac8
Maurice Claessen <mc1984@gmail.com>
2023-09-25 21:00:10 +0200

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanString = string.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
 
+    for (var i=0; i<cleanString.length;i++) {
+        if (cleanString[i] != cleanString[cleanString.length - (i + 1)]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
