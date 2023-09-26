9dd72736b7230364c7f64ec228c030bb9a7adcc8
Maurice Claessen <mc1984@gmail.com>
2023-09-26 17:48:01 +0200

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = null;
 
+    for (person of people) {
+        if (oldest == null)
+            oldest = person;
+
+        const personYod = person.yearOfDeath == undefined ? (new Date(Date.now())).getFullYear() : person.yearOfDeath;
+        const personAge = personYod - person.yearOfBirth;
+        const oldestYod = oldest.yearOfDeath == undefined ? (new Date(Date.now())).getFullYear() : oldest.yearOfDeath;
+        const oldestAge = oldestYod - oldest.yearOfBirth;
+
+        if (oldestAge < personAge)
+            oldest = person;
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
